0acfb27e13beedc09053ab9f95cc8eafd16b2080
Navel Development <contact@navel.dev>
2020-11-12 20:05:05 +0100

    🕹️ Console Commands can be added through $commands instead of config/commands

--- application/Console/Kernel.php
+++ application/Console/Kernel.php
@@ -6,4 +6,12 @@ use Navel\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    /**
+     * The commands
+     *
+     * @var array
+     */
+    protected $commands = [
+        Navel\Foundation\Console\AnotherCommand::class,
+    ];
 }
--- application/configuration/console.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-
-    'commands' => [
-        Navel\Foundation\Console\TestCommand::class,
-    ],
-
-];
